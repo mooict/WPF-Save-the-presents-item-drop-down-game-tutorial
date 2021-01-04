f0c0b478aa384e9be202a7903853aa48d364de0c
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-04 00:35:58 +0000

    Add files via upload

---
+++ Save the Presents MOO ICT WPF/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Save_the_Presents_MOO_ICT_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
